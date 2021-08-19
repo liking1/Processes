760a56a64015e0f8de6cafff69edb8083040b028
liking1 <liking1>
2021-08-19 20:54:53 +0300

    Pr

---
+++ Processes/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; // for working with processes
+using System.Reflection;        
+
+namespace Processes
+{
+    class Program
+    {
+        static void NextExample()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main(string[] args)
+        {
+            //////////////// Current Process
+            Process current = Process.GetCurrentProcess();
+            
+
+            // Process Priority:
+            // * Idle
+            // * BelowNormal
+            // * Normal (def)
+            // * AboveNormal
+            // * High
+            // * RealTime (only set by OS)
+            // */
+            current.PriorityClass = ProcessPriorityClass.High;
+
+            //////////////////// Process Info
+            Console.WriteLine("----------- Current proccess info ------------");
+            
+            Console.WriteLine("Priority class: " + current.PriorityClass);
+            Console.WriteLine("Name: " + current.ProcessName);
+            Console.WriteLine("Id: " + current.Id);
+            Console.WriteLine("MachineName: " + current.MachineName);
+            Console.WriteLine("PrivateMemorySize (KB): " + current.PrivateMemorySize64 / 1024);
+            Console.WriteLine("StartTime: " + current.StartTime);
+            Console.WriteLine("TotalProcessorTime: " + current.TotalProcessorTime);
+            Console.WriteLine("UserProcessorTime: " + current.UserProcessorTime);
+            NextExample();
+
+            //////////////////// All Processes
+            Process[] processes = Process.GetProcesses();
+
+            Console.WriteLine("Process Name\t\t\tPID\t\t\tPriority\tMachine name");
+            Console.WriteLine("___________________________________________");
+            foreach (var p in processes)
+            {
+                Console.WriteLine($"{p.ProcessName}\t{p.Id}\t{p.BasePriority}\t{p.MachineName}");
+            }
+            NextExample();
+
+            ///////////////// Start Process
+
+            Process.Start("MicrosoftEdge.exe", "stackoverflow.com google.com");
+            NextExample();
+
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = "notepad.exe",
+                Arguments = @"C:\Users\tlvla\Desktop\777.txt",
+                WindowStyle = ProcessWindowStyle.Maximized,
+            };
+            Process pr = Process.Start(info);
+
+            Console.WriteLine("Press key to do operation...");
+            Console.ReadKey();
+            //////////////////// Process Methods
+            //pr.Close();           
+            //pr.Refresh();           // clear cashe
+            pr.CloseMainWindow();  // close process by normal mode = Alt+F4
+            //pr.Kill();              // imediatelly stops a process = End Task
+            Console.WriteLine("Operation has done!");
+
+            Console.WriteLine("Wait for exit...");
+            pr.WaitForExit(); // wait until proccess runing
+            Console.WriteLine("Process was exited...");
+            Console.WriteLine("ExitCode: " + pr.ExitCode);
+            Console.WriteLine("ExitTime: " + pr.ExitTime);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer _timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //_timer = new DispatcherTimer();
+            //_timer.Interval = new TimeSpan(0, 0, 5);
+            //_timer.Tick += _timer_Tick;
+            //_timer.Start();
+            Load();
+        }
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            Load();
+        }
+        public void Load()
+        {
+            //grid.ItemsSource = Process.GetProcesses();//.Select(p => new ProcessInfo(p));
+            grid.ItemsSource = Process.GetProcesses();
+            int[] arr = new int[] { 1, 2, 5 };
+            cbIntervals.ItemsSource = arr;
+        }
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0, (int)cbIntervals.SelectedItem);
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+        private void btnKill_Click(object sender, RoutedEventArgs e)
+        {
+            var processKill = Process.GetProcesses().Where(p => p.ProcessName == grid.SelectedItem);
+            foreach (var item in processKill)
+            {
+                item.Kill();
+            }
+        }
+        private void btnNewProcess_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(tbArea.Text);
+        }
+    }
+
+    class ProcessInfo
+    {
+        public int Id { get; set; }
+        public TimeSpan TotalProcessorTime { get; set; }
+        public ProcessPriorityClass PriorityClass { get; set; }
+        public string UserName { get; set; }
+        public bool State { get; set; }
+
+        public ProcessInfo(Process pr)
+        {
+            Id = pr.Id;
+            try { TotalProcessorTime = pr.TotalProcessorTime; } catch { }
+            try { PriorityClass = pr.PriorityClass; } catch { }
+            UserName = GetProcessOwner(pr.Id);
+            try { State = pr.HasExited; } catch { }
+        }
+        public string GetProcessOwner(int processId)
+        {
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return DOMAIN\user
+                    return argList[0];
+                }
+            }
+            return "NO OWNER";
+        }
+    }
+}
